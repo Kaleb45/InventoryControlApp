b9f70d2f5182c3e897d91f45f29095c59a1fc806
Civer <mausan.bedolla@gmail.com>
2023-10-25 07:54:16 -0600

    Add logout, Add Almacenista.cs, Add Coordinador,cs

--- UI.cs
+++ UI.cs
@@ -19,6 +19,9 @@ public static class UI
                 case "2":
                     SignUp();
                     break;
+                default:
+                    Console.WriteLine("Opçión no valida");
+                    break;
             }
         }
     }
@@ -74,8 +77,17 @@ public static class UI
             Console.WriteLine("1: Solicitar un material");
             Console.WriteLine("2: Ver solicitudes");
             Console.WriteLine("3: Cambiar contraseña");
+            Console.WriteLine("9: Logout");
             String option = Console.ReadLine();
             Console.Clear();
+
+            switch (option)
+            {
+                case "9":
+                    return;
+                default:
+                    break;
+            }
         } while (true);
     }
 
@@ -88,8 +100,17 @@ public static class UI
             Console.WriteLine("2: Ver solicitudes");
             Console.WriteLine("3: Hacer una solicitud");
             Console.WriteLine("4: Cambiar contraseña");
+            Console.WriteLine("9: Logout");
             String option = Console.ReadLine();
             Console.Clear();
+
+            switch (option)
+            {
+                case "9":
+                    return;
+                default:
+                    break;
+            }
         } while (true);
     }
 
@@ -102,8 +123,17 @@ public static class UI
             Console.WriteLine("2: Ver solicitudes");
             Console.WriteLine("3: Generar informes");
             Console.WriteLine("4: Cambiar contraseña");
+            Console.WriteLine("9: Logout");
             String option = Console.ReadLine();
             Console.Clear();
+
+            switch (option)
+            {
+                case "9":
+                    return;
+                default:
+                    break;
+            }
         } while(true);
     }
 
@@ -116,8 +146,17 @@ public static class UI
             Console.WriteLine("2: Opción 2");
             Console.WriteLine("3: Opción 3");
             Console.WriteLine("4: Cambiar contraseña");
+            Console.WriteLine("9: Logout");
             String option = Console.ReadLine();
             Console.Clear();
+            
+            switch (option)
+            {
+                case "9":
+                    return;
+                default:
+                    break;
+            }
         } while(true);
     }
 

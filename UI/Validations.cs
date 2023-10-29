2bb560867713c637d220a939663136b0129d1554
Kaleb45 <a20300663@ceti.mx>
2023-10-29 12:42:46 -0600

    Changed Gretta SignUp

--- UI/Validations.cs
+++ UI/Validations.cs
@@ -66,25 +66,86 @@ public static partial class UI
     {
         if (password.Length < 8)
         {
+            Console.WriteLine("La contraseña es muy corta");
             return 10;
         }
 
         if (!password.Any(char.IsUpper))
         {
+            Console.WriteLine("La contraseña debe contener al menos un caracter en mayusculas");
             return 20;
         }
 
         if (!password.Any(char.IsDigit))
         {
+            Console.WriteLine("La contraseña debe contener al menos un caracter numerico");
             return 30;
         }
 
         if (!password.Any(c => "!@#$%^&*()-_+=<>?".Contains(c)))
         {
+            Console.WriteLine("La contraseña debe contener al menos un caracter especial");
             return 40;
         }
 
         return 01;
+    }
+
+    public static string GenerateUsername(string nombre, string ApellidoPaterno, string ApellidoMaterno)
+    {
+        string primerNombre = nombre.Split(' ')[0]; // Obtén el primer nombre
+
+        string segundoNombre = string.Empty;
+        if (nombre.Split(' ').Length > 1)
+        {
+            segundoNombre = nombre.Split(' ')[1][0].ToString(); // Obtiene la primera letra del segundo nombre si existe
+        }
+
+        string primerApellido = ApellidoPaterno;
+        string segundoApellido = ApellidoMaterno[0].ToString(); // Obtén la primera letra del segundo apellido
+
+        string username = $"{primerNombre}{segundoNombre}{primerApellido}{segundoApellido}";
+
+        return username;
+    }
+
+    public static string SecondOptionUsername(string nombre, string ApellidoPaterno, string ApellidoMaterno)
+    {
+        string primerNombre = nombre.Split(' ')[0]; // Obtén el primer nombre
+
+        string segundoNombre = string.Empty;
+        if (nombre.Split(' ').Length > 1)
+        {
+            segundoNombre = nombre.Split(' ')[1][0].ToString(); // Obtiene la primera letra del segundo nombre si existe
+        }
+
+        string username = $"{primerNombre}{segundoNombre}{ApellidoPaterno}{ApellidoMaterno}";
+
+        return username;
+    }
+
+    public static bool NameValidation(string name){
+        bool validName;
+        if(name.Any(n => "!1234567890".Contains(n)) || name.Any(n => "!@#$%^&*()-_+=<>?".Contains(n))){
+            Console.WriteLine("No debe contener numeros o carcateres especiales");
+            validName = false;
+        }
+        else{
+            validName = true;
+        }
+        return validName;
+    }
+
+    public static bool EmailValidation(string email){
+        bool validEmail;
+        if(email.Contains("@ceti.mx") || email.Contains("@live.ceti.mx")){
+            validEmail = true;
+        }
+        else{
+            Console.WriteLine("Correo electrónico invalido");
+            validEmail = false;
+        }
+        return validEmail;
     }
 
     /// TODO:

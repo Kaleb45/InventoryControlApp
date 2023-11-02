6846a234b09389fb46a5b2ba0e24c70360c9f9fb
MisaelCueRab <142126523+MisaelCueRab@users.noreply.github.com>
2023-11-02 00:12:05 -0600

    Divide SignUp in diferent functions

--- UI/SignUp.cs
+++ UI/SignUp.cs
@@ -7,7 +7,13 @@ public static partial class UI
     {
         int Registro;
         string? Nombre;
+        string? ApellidoPaterno;
+        string? ApellidoMaterno;
         int plantel;
+        int Semestre;
+        int Grupo;
+        string? Correo;
+        string? Contrasena;
         do
         {
             WriteLine("Ingresa tu registro");
@@ -20,22 +26,21 @@ public static partial class UI
         } while (NameValidation(Nombre) == false);
 
         WriteLine("Ingresa tu Apellido Paterno");
-        string? ApellidoPaterno = ReadLine();
-        while (NameValidation(ApellidoPaterno) == false)
+
+        do
         {
             ApellidoPaterno = ReadLine();
-        }
+        } while (NameValidation(ApellidoPaterno) == false);
 
         WriteLine("Ingresa tu Apellido Materno");
-        string? ApellidoMaterno = ReadLine();
-        while (NameValidation(ApellidoMaterno) == false)
+        do
         {
             ApellidoMaterno = ReadLine();
-        }
+        } while (NameValidation(ApellidoMaterno) == false);
 
         string newUsername = GenerateUsername(Nombre, ApellidoPaterno, ApellidoMaterno);
 
-        
+
         do
         {
             WriteLine("Plantel: ");
@@ -48,37 +53,34 @@ public static partial class UI
             }
         } while (plantel < 1 || plantel > 3);
 
-        WriteLine("Ingresa tu Semestre");
-        int Semestre;
-        while (int.TryParse(ReadLine(), out Semestre) == false || Semestre < 1 || Semestre > 8)
-        {
-            WriteLine("Caracteres invalidos en Semestre");
-        }
 
-        WriteLine("Ingresa tu Grupo");
-        int Grupo;
-        while (int.TryParse(ReadLine(), out Grupo) == false || Grupo < 1 || Grupo > 26)
+        do
         {
-            WriteLine("Grupo no existente");
-        }
+            WriteLine("Ingresa tu Semestre");
+        } while (int.TryParse(ReadLine(), out Semestre) == false || Semestre < 1 || Semestre > 8);
 
-        WriteLine("Ingesa tu correo");
-        string? Correo = ReadLine();
-        while (StudentEmailValidation(Correo, Registro) != 01)
+        do
         {
-            WriteLine("Correo con formato incorrecto");
+            WriteLine("Ingresa tu Grupo");
+        } while (int.TryParse(ReadLine(), out Grupo) == false || Grupo < 1 || Grupo > 26);
+
+        do
+        {
+            WriteLine("Ingesa tu correo");
             Correo = ReadLine();
-        }
+        } while (StudentEmailValidation(Correo, Registro) != 01);
 
-        WriteLine("Ingresa tu contraseña");
-        string? Contrasena = ReadLine();
-        while (PasswordValidation(Contrasena) != 01)
+        do
         {
-            WriteLine("Contraseña con formato incorrecto");
+            WriteLine("Ingresa tu contraseña");
             Contrasena = ReadLine();
-        }
+        } while (PasswordValidation(Contrasena) != 01);
 
+        AddStudent(Registro, newUsername, Nombre, ApellidoPaterno, ApellidoMaterno, plantel, Semestre, Grupo, Correo, Contrasena);
+    }
 
+    public static void AddStudent(int Registro, string newUsername, string? Nombre, string? ApellidoPaterno, string? ApellidoMaterno, int plantel, int Semestre, int Grupo, string? Correo, string? Contrasena)
+    {
         using (Almacen db = new Almacen())
         {
             var CheckStudent = db.Estudiantes.FirstOrDefault(r => r.EstudianteId == Registro || r.Correo == Correo);
@@ -117,77 +119,83 @@ public static partial class UI
 
         }
     }
+
     public static void SignUpDocente()
     {
-       using (Almacen db = new Almacen()){
+        string nombre;
+        string ApellidoPaterno;
+        string ApellidoMaterno;
+        string correo;
+        int plantel;
+        string password;
+        int validacionPassword;
+        string newUsername;
+
+        do
+        {
+            Console.Write("Nombre: ");
+            nombre = Console.ReadLine();
+        } while (!NameValidation(nombre));
+
+        do
+        {
+            Console.Write("Apellido Paterno: ");
+            ApellidoPaterno = Console.ReadLine();
+        } while (!NameValidation(ApellidoPaterno));
+
+        do
+        {
+            Console.Write("Apellido Materno: ");
+            ApellidoMaterno = Console.ReadLine();
+        } while (!NameValidation(ApellidoMaterno));
+
+        newUsername = GenerateUsername(nombre, ApellidoPaterno, ApellidoMaterno);
+
+        do
+        {
+            Console.Write("Correo: ");
+            correo = Console.ReadLine();
+        } while (!EmailValidation(correo));
+
+        do
+        {
+            Console.WriteLine("Plantel: ");
+            Console.WriteLine("1. Colomos");
+            Console.WriteLine("2. Tonalá");
+            Console.WriteLine("3. Río Santiago");
+            if (!int.TryParse(Console.ReadLine(), out plantel))
+            {
+                Console.WriteLine("Opción invalida");
+            }
+            else
+            {
+                plantel = int.Parse(Console.ReadLine());
+            }
+
+        } while (plantel < 1 || plantel > 3);
+
+        do
+        {
+            Console.Write("Contraseña: ");
+            password = Console.ReadLine();
+            validacionPassword = PasswordValidation(password);
+        } while (validacionPassword != 01);
+
+        AddTeacher(newUsername, nombre, ApellidoPaterno, ApellidoMaterno, plantel, correo, password);
+    }
+
+    public static void AddTeacher(string newUsername, string? nombre, string? ApellidoPaterno, string? ApellidoMaterno, int plantel, string? correo, string? password)
+    {
+        using (Almacen db = new Almacen())
+        {
+            var CheckTeacher = db.Docentes.FirstOrDefault(r => r.Correo == correo);
+            if (CheckTeacher != null)
+            {
+                WriteLine("Datos de usuario ya existentes");
+                return;
+            }
             int? lastUserId = db.Usuarios.OrderByDescending(u => u.UsuarioId).Select(u => u.UsuarioId).FirstOrDefault();
             int newId = lastUserId.HasValue ? lastUserId.Value + 1 : 1;
-            string nombre;
-            string ApellidoPaterno;
-            string ApellidoMaterno;
-            string correo;
-            int plantel;
-            string password;
-            int validacionPassword;
-            bool sameDocente = false;
-            string newUsername;
-
-            do{
-                Console.Write("Nombre: ");
-                nombre = Console.ReadLine();
-            }while(!NameValidation(nombre));
-
-            do{
-                Console.Write("Apellido Paterno: ");
-                ApellidoPaterno = Console.ReadLine();
-            }while(!NameValidation(ApellidoPaterno));
-
-            do{
-                Console.Write("Apellido Materno: ");
-                ApellidoMaterno = Console.ReadLine();
-            }while(!NameValidation(ApellidoMaterno));
-
-            newUsername = GenerateUsername(nombre, ApellidoPaterno, ApellidoMaterno);
-
-            do{
-                Console.Write("Correo: ");
-                correo = Console.ReadLine();
-                foreach(Docente docente in db.Docentes){
-                    if(correo == docente.Correo){
-                        Console.WriteLine("Usuario ya registrado");
-                        sameDocente = true;
-                    }
-                    else{
-                        foreach(Usuario usuario in db.Usuarios){
-                            if(newUsername == usuario.Usuario1){
-                                //En caso de que existan dos personas con el mismo nombre
-                                newUsername = SecondOptionUsername(nombre, ApellidoPaterno, ApellidoMaterno);
-                            }
-                        }
-                        sameDocente = false;
-                    }
-                }
-            }while(!EmailValidation(correo) || sameDocente);
-
-            do{
-                Console.WriteLine("Plantel: ");
-                Console.WriteLine("1. Colomos");
-                Console.WriteLine("2. Tonalá");
-                Console.WriteLine("3. Río Santiago");
-                if(!int.TryParse(Console.ReadLine(), out plantel)){
-                    Console.WriteLine("Opción invalida");
-                }
-                else{
-                    plantel = int.Parse(Console.ReadLine());
-                }
-
-            }while(plantel < 1 || plantel > 3);
-            
-            do{
-                Console.Write("Contraseña: ");
-                password = Console.ReadLine();
-                validacionPassword = PasswordValidation(password);
-            }while(validacionPassword != 01);
 
             Usuario newUser = new Usuario
             {
@@ -199,7 +207,8 @@ public static partial class UI
             int? lastDocenteId = db.Docentes.OrderByDescending(d => d.DocenteId).Select(d => d.DocenteId).FirstOrDefault();
             int newDocenteId = lastDocenteId.HasValue ? lastDocenteId.Value + 1 : 1;
 
-            Docente newDocente = new Docente{
+            Docente newDocente = new Docente
+            {
                 DocenteId = newDocenteId,
                 Nombre = nombre,
                 ApellidoPaterno = ApellidoPaterno,
@@ -212,15 +221,11 @@ public static partial class UI
             db.Usuarios.Add(newUser);
             db.Docentes.Add(newDocente);
             db.SaveChanges();
-            
-       } 
+        }
     }
 
     public static void SignUpAlmacenista()
     {
-       using (Almacen db = new Almacen()){
-            int? lastUserId = db.Usuarios.OrderByDescending(u => u.UsuarioId).Select(u => u.UsuarioId).FirstOrDefault();
-            int newId = lastUserId.HasValue ? lastUserId.Value + 1 : 1;
             string nombre;
             string ApellidoPaterno;
             string ApellidoMaterno;
@@ -228,65 +233,73 @@ public static partial class UI
             int plantel;
             string password;
             int validacionPassword;
-            bool sameAlmacenista = false;
             string newUsername;
 
-            do{
+            do
+            {
                 Console.Write("Nombre: ");
                 nombre = Console.ReadLine();
-            }while(!NameValidation(nombre));
+            } while (!NameValidation(nombre));
 
-            do{
+            do
+            {
                 Console.Write("Apellido Paterno: ");
                 ApellidoPaterno = Console.ReadLine();
-            }while(!NameValidation(ApellidoPaterno));
+            } while (!NameValidation(ApellidoPaterno));
 
-            do{
+            do
+            {
                 Console.Write("Apellido Materno: ");
                 ApellidoMaterno = Console.ReadLine();
-            }while(!NameValidation(ApellidoMaterno));
+            } while (!NameValidation(ApellidoMaterno));
 
             newUsername = GenerateUsername(nombre, ApellidoPaterno, ApellidoMaterno);
 
-            do{
+            do
+            {
                 Console.Write("Correo: ");
                 correo = Console.ReadLine();
-                foreach(Almacenista almacenista in db.Almacenistas){
-                    if(correo == almacenista.Correo){
-                        Console.WriteLine("Usuario ya registrado");
-                        sameAlmacenista = true;
-                    }
-                    else{
-                        foreach(Usuario usuario in db.Usuarios){
-                            if(newUsername == usuario.Usuario1){
-                                //En caso de que existan dos personas con el mismo nombre
-                                newUsername = SecondOptionUsername(nombre, ApellidoPaterno, ApellidoMaterno);
-                            }
-                        }
-                        sameAlmacenista = false;
-                    }
-                }
-            }while(!EmailValidation(correo) || sameAlmacenista);
+            } while (!EmailValidation(correo));
 
-            do{
+            do
+            {
                 Console.WriteLine("Plantel: ");
                 Console.WriteLine("1. Colomos");
                 Console.WriteLine("2. Tonalá");
                 Console.WriteLine("3. Río Santiago");
-                if(!int.TryParse(Console.ReadLine(), out plantel)){
+                if (!int.TryParse(Console.ReadLine(), out plantel))
+                {
                     Console.WriteLine("Opción invalida");
                 }
-                else{
+                else
+                {
                     plantel = int.Parse(Console.ReadLine());
                 }
 
-            }while(plantel < 1 || plantel > 3);
-            
-            do{
+            } while (plantel < 1 || plantel > 3);
+
+            do
+            {
                 Console.Write("Contraseña: ");
                 password = Console.ReadLine();
                 validacionPassword = PasswordValidation(password);
-            }while(validacionPassword != 01);
+            } while (validacionPassword != 01);
+
+            AddTeacher(newUsername, nombre, ApellidoPaterno, ApellidoMaterno, plantel, correo, password);
+    }
+
+    public static void AddWarehouseManager(string newUsername, string? nombre, string? ApellidoPaterno, string? ApellidoMaterno, int plantel, string? correo, string? password)
+    {
+        using (Almacen db = new Almacen())
+        {
+            var CheckWarehouseManager = db.Almacenistas.FirstOrDefault(r => r.Correo == correo);
+            if (CheckWarehouseManager != null)
+            {
+                WriteLine("Datos de usuario ya existentes");
+                return;
+            }
+            int? lastUserId = db.Usuarios.OrderByDescending(u => u.UsuarioId).Select(u => u.UsuarioId).FirstOrDefault();
+            int newId = lastUserId.HasValue ? lastUserId.Value + 1 : 1;
 
             Usuario newUser = new Usuario
             {
@@ -298,7 +311,8 @@ public static partial class UI
             int? lastAlmacenistaId = db.Almacenistas.OrderByDescending(a => a.AlmacenistaId).Select(a => a.AlmacenistaId).FirstOrDefault();
             int newAlmacenistaId = lastAlmacenistaId.HasValue ? lastAlmacenistaId.Value + 1 : 1;
 
-            Almacenista newAlmacenista = new Almacenista{
+            Almacenista newAlmacenista = new Almacenista
+            {
                 AlmacenistaId = newAlmacenistaId,
                 Nombre = nombre,
                 ApellidoPaterno = ApellidoPaterno,
@@ -311,7 +325,7 @@ public static partial class UI
             db.Usuarios.Add(newUser);
             db.Almacenistas.Add(newAlmacenista);
             db.SaveChanges();
-            
-       } 
+
+        }
     }
 }

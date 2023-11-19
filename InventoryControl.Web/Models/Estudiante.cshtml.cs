351b5f59cfa90b4303e6928546107756df61a563
Diego Go <143672544+Dianroan@users.noreply.github.com>
2023-11-19 13:53:50 -0600

    Student page is funtional

--- InventoryControl.Web/Models/Estudiante.cshtml.cs
+++ InventoryControl.Web/Models/Estudiante.cshtml.cs
@@ -24,7 +24,11 @@ namespace InventoryControlPages
 
         [BindProperty]
         public Estudiante? estudiante { get; set; }
+        [BindProperty]
+        public Pedido? pedido { get; set; } = new();
 
+        [BindProperty]
+        public DescPedido? descPedido { get; set; }
         public void OnGet(int id)
         {
             estudiante = db.Estudiantes.FirstOrDefault(e => e.EstudianteId == id);
@@ -33,9 +37,13 @@ namespace InventoryControlPages
 
         public IActionResult OnPost()
         {
-            if (ModelState.IsValid)
+            if ((pedido is not null) && (descPedido is not null) &&  !ModelState.IsValid)
             {
-                
+                WriteLine($"{pedido.EstudianteId}");
+                //WriteLine($" {pedido.Fecha,-22} | | {pedido.HoraEntrega,-22} | {pedido.HoraDevolucion,-5}");
+                CrudFuntions.AddPedido(pedido, descPedido);
+                TempData["UserType"] = 2;
+                return Page();
             }
             return Page();
         }

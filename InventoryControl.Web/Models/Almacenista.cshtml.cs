10632596c10aa0cdc87edeb2fef4c4695787615c
Diego Go <143672544+Dianroan@users.noreply.github.com>
2023-11-20 00:22:03 -0600

    Almacenista page reads and creates are funtional

--- InventoryControl.Web/Models/Almacenista.cshtml.cs
+++ InventoryControl.Web/Models/Almacenista.cshtml.cs
@@ -22,13 +22,32 @@ namespace InventoryControlPages
 
         public List<Almacenista>? almacenistas { get; set; }
 
-        public void OnGet()
-        {
-            ViewData["Title"] = "";
-        }
+        [BindProperty]
+        public Almacenista? almacenista { get; set; }
 
         [BindProperty]
-        public Almacenista? Almacenista { get; set; }
+        public Pedido? pedido { get; set; }
+
+        [BindProperty]
+        public DescPedido? descPedido { get; set; }
+
+        [BindProperty]
+        public Categoria? categoria { get; set; }
+
+        [BindProperty]
+        public Material? material { get; set; }
+
+        [BindProperty]
+        public Marca? marca { get; set; }
+
+        [BindProperty]
+        public Modelo? modelo { get; set; }
+
+        public void OnGet(int id)
+        {
+            almacenista = db.Almacenistas.FirstOrDefault(a => a.AlmacenistaId == id);
+            ViewData["Title"] = "";
+        }
 
         public IActionResult OnPost()
         {
@@ -38,6 +57,91 @@ namespace InventoryControlPages
             }
             
             return Page();
+        }
+
+        public IActionResult OnPostApprove()
+        {
+            // Obtener el valor de pedidoId del formulario
+            pedido = db.Pedidos.FirstOrDefault(p => p.PedidoId == int.Parse(Request.Form["pedidoId"]));
+            pedido.Estado = true;
+            Estudiante estudiante = db.Estudiantes.FirstOrDefault(e => e.EstudianteId == pedido.EstudianteId);
+            UI.SendEmailForOrderState(estudiante,"Datos incorrectos",pedido);
+            db.SaveChanges();
+            TempData["UserType"] = 3;
+            return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
+        }
+
+        public IActionResult OnPostDecline()
+        {
+            // Obtener el valor de pedidoId del formulario            
+            pedido = db.Pedidos.FirstOrDefault(p => p.PedidoId == int.Parse(Request.Form["pedidoId"]));
+            pedido.Estado = false;
+            descPedido = db.DescPedidos.FirstOrDefault(p => p.PedidoId == pedido.PedidoId);
+            Estudiante estudiante = db.Estudiantes.FirstOrDefault(e => e.EstudianteId == pedido.EstudianteId);
+            UI.SendEmailForOrderState(estudiante,"Datos incorrectos",pedido);
+            db.DescPedidos.RemoveRange(descPedido);
+            db.Pedidos.RemoveRange(pedido);
+            db.SaveChanges();
+            TempData["UserType"] = 3;
+            return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
+        }
+
+        public IActionResult OnPostNewCat()
+        {
+            // Obtener el valor de pedidoId del formulario  
+            if ((categoria is not null) &&  !ModelState.IsValid)
+            {
+                CrudFuntions.AddCategoria(categoria);
+                TempData["UserType"] = 3;
+                return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostNewMarca()
+        {
+            // Obtener el valor de pedidoId del formulario  
+            if ((marca is not null) &&  !ModelState.IsValid)
+            {
+                CrudFuntions.AddMarca(marca);
+                TempData["UserType"] = 3;
+                return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostNewModelo()
+        {
+            // Obtener el valor de pedidoId del formulario  
+            if ((modelo is not null) &&  !ModelState.IsValid)
+            {
+                CrudFuntions.AddModelo(modelo);
+                TempData["UserType"] = 3;
+                return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
+            }
+            return Page();
+        }
+        public IActionResult OnPostNewMaterial()
+        {
+            // Obtener el valor de pedidoId del formulario  
+            if ((material is not null) &&  !ModelState.IsValid)
+            {
+                CrudFuntions.AddMaterial(material);
+                TempData["UserType"] = 3;
+                return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
+            }
+            return Page();
+        }
+        public IActionResult OnPostDeleteOrder()
+        {
+            // Obtener el valor de pedidoId del formulario            
+            pedido = db.Pedidos.FirstOrDefault(p => p.PedidoId == int.Parse(Request.Form["pedidoId"]));
+            descPedido = db.DescPedidos.FirstOrDefault(p => p.PedidoId == pedido.PedidoId);
+            db.DescPedidos.RemoveRange(descPedido);
+            db.Pedidos.RemoveRange(pedido);
+            db.SaveChanges();
+            TempData["UserType"] = 3;
+            return RedirectToPage("/AlmacenistaMenu", new{id = int.Parse(Request.Form["almacenistaId"])});
         }
     }
 }
